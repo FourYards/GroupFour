3bd90c6e7761e62003baafb9148afa7b504aab16
Keaton Anderson <A02348614@usu.edu>
2022-02-24 11:33:54 -0700

    Add user profile and settings paths

--- routes/routes.js
+++ routes/routes.js
@@ -6,11 +6,13 @@ const usersRouter = require('./users');
 const browseRouter = require('./browse');
 const dashboardRouter = require('./dashboard');
 const loginRouter = require('./login');
+const userRouter = require('./user/routes');
 
 router.use('/', indexRouter);
 router.use('/about', usersRouter);
 router.use('/browse', browseRouter);
 router.use('/dashboard', dashboardRouter);
 router.use('/login', loginRouter);
+router.use('/user', userRouter);
 
 module.exports = router
