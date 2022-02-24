f7f039495234d840409e6cc79fa8e7ed6ce2fabd
Keaton Anderson <A02348614@usu.edu>
2022-02-24 11:19:32 -0700

    Create login router

--- routes/routes.js
+++ routes/routes.js
@@ -5,10 +5,12 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const listingsRouter = require('./routes/listings');
 const dashboardRouter = require('./routes/dashboard');
+const loginRouter = require('./routes/login');
 
 router.use('/', indexRouter);
 router.use('/about', usersRouter);
 router.use('/listings', listingsRouter);
 router.use('/dashboard', dashboardRouter);
+router.use('/login', loginRouter);
 
 module.exports = router
