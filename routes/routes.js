dd25c3f1636cb30d7b63d15193ab916b9b4c29d0
Keaton Anderson <A02348614@usu.edu>
2022-02-24 11:13:15 -0700

    Add route to view all listings

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const listingsRouter = require('./routes/listings');
 
-router.use('/', indexRouter)
-router.use('/about', usersRouter)
+router.use('/', indexRouter);
+router.use('/about', usersRouter);
+router.use('/listings', listingsRouter);
 
 module.exports = router
