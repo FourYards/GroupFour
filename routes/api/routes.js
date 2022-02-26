8167b0801f96794017fed2098b2cada599ac7bab
Keatonan <keatona379@gmail.com>
2022-02-26 00:16:32 -0700

    Add routes for creating, updating, and accessing users

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -4,9 +4,11 @@ var router = express.Router();
 const bidRouter = require('./bid');
 const balanceRouter = require('./balance');
 const listingRouter = require('./listing');
+const userRouter = require('./user');
 
 router.use('/bid', bidRouter);
 router.use('/balance', balanceRouter);
 router.use('/listing', listingRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
---
+++ routes/api/user.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.send('This URL will handle POST, PUT, and DELETE requests relating to user accounts.');
+});
+
+/* POST a new User. */
+router.post('/', (req, res, next) => {
+  res.send('This endpoint will try to create a new user.');
+});
+
+/* PUT a new User. */
+router.put('/', (req, res, next) => {
+  res.send('This endpoint will update the users information.');
+});
+
+module.exports = router;
