d63f815799f0d1055016bb0fb763884f7e38832a
Keatonan <keatona379@gmail.com>
2022-02-26 00:20:40 -0700

    Update the bid api endpoint

--- routes/api/bid.js
+++ routes/api/bid.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET one or many bids. */
 router.get('/', (req, res, next) => {
   res.send('This URL will respond to GET, POST, and DELETE requests relating to bids.');
 });
+
+/* POST a new bid. */
+router.post('/', (req, res, next) => {
+  res.send('This endpoint will create a new bid on a listing.');
+});
+
+/* DELETE a bid. */
+router.delete('/', (req, res, next) => {
+  res.send('This endpoint will delete a bid on a listing.');
+});
+
 
 module.exports = router;
