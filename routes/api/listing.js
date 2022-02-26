50550cb1d5d9a2025fe07ebc8303285037431703
Keatonan <keatona379@gmail.com>
2022-02-26 00:23:00 -0700

    Update the listing api endpoint

--- routes/api/listing.js
+++ routes/api/listing.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET one or many listings. */
 router.get('/', (req, res, next) => {
   res.send('This URL will respond to GET, POST, PUT, and DELETE requests regarding listings.');
 });
+
+/* POST a new listing. */
+router.post('/', (req, res, next) => {
+  res.send('This endpoint will create a new listing.');
+});
+
+/* PUT new info on a listing. */
+router.put('/', (req, res, next) => {
+  res.send('This endpoint will be used to update the listing status.');
+});
+
+/* DELETE a listing. */
+router.delete('/', (req, res, next) => {
+  res.send('This endpoint will be used to delete listings.');
+});
+
 
 module.exports = router;
