6f316fd1e4f9b4c852dd37c008cd72c0e678a837
Kytech <Kytech@users.noreply.github.com>
2022-03-19 17:40:46 -0600

    Create seed for user roles.

---
+++ server/db/seeders/20220319231156-roles.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'Roles',
+      [
+        {
+          name: 'Administrator',
+          code: 'ADM',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'User',
+          code: 'USR',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Roles', null, {});
+  },
+};
