77db4c140b2ab490b21e70ead6989ac6e7112631
Wesley Perrett <wesley.g.perrett@gmail.com>
2022-03-19 21:56:18 -0600

    update to bootstrap config

--- client/page/vue.js
+++ client/page/vue.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import VueLandingPage from './Vue.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   el: '#vue-root-component',
