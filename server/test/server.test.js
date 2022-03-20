b62ce25eccf923ad5cc15a9627c933c98c07746f
Keatonan <keatona379@gmail.com>
2022-03-20 16:35:59 -0600

    Fix supertest setup and create a test to attempt to get an index page

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,16 +1,14 @@
-/**
- * I am not certain if this testing works yet, this file should test that my supertest is setup
- */
 const supertest = require('supertest');
 const createServer = require('./testserver');
 
 const app = createServer();
 
-test('GET /api/balance', async () => {
+// See that we are getting an index page
+test('GET /', async () => {
   await supertest(app)
-    .get('/api/balance')
+    .get('/')
     .expect(200)
     .then((response) => {
-      console.log('We got this as the response to URL/' + response.body);
+      console.log('We recieved a response, ' + response);
     });
 });
--- server/test/testserver.js
+++ server/test/testserver.js
@@ -1,10 +1,43 @@
+const createError = require('http-errors');
 const express = require('express');
-const apiRouter = require('../routes/api/routes');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+const hbsHelpers = require('../views/helpers');
+const indexRouter = require('../routes/routes');
 
 function createServer() {
   const app = express();
+
+  app.set('views', path.join(__dirname, 'server', 'views'));
+  app.set('view engine', 'hbs');
+  hbs.localsAsTemplateData(app);
+  hbsHelpers(hbs);
+
+  app.use(logger('dev'));
   app.use(express.json());
-  app.use('/', apiRouter);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'client', 'public')));
+
+  app.use('/', indexRouter);
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
   return app;
 }
 
