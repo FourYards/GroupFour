d8a2a690da759fa9c8acdb290474b6492ca2bf94
Keatonan <keatona379@gmail.com>
2022-03-20 17:40:55 -0600

    Add tests for all desired pages. Group tests better.

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -3,13 +3,15 @@ const createServer = require('./testserver');
 
 const app = createServer();
 
-// Test to get a user's balance
-test('GET /api/balance', async () => {
-  await supertest(app)
-    .get('/api/balance')
-    .expect(200)
-    .then((response) => {
-      console.log('We recieved a user balance of, ' + response.body.balance);
-      expect(response.body.balance).toBe(0); // this instance is not signed in so it should have no balance
-    });
+describe('API endpoints', () => {
+  // Test to get a user's balance
+  test('GET /api/balance', async () => {
+    await supertest(app)
+      .get('/api/balance')
+      .expect(200)
+      .then((response) => {
+        console.log('We recieved a user balance of, ' + response.body.balance);
+        expect(response.body.balance).toBe(0); // this instance is not signed in so it should have no balance
+      });
+  });
 });
--- server/test/router.test.js
+++ server/test/router.test.js
@@ -3,12 +3,29 @@ const createServer = require('./testserver');
 
 const app = createServer();
 
-// See that we are getting an index page
-test('GET /', async () => {
-  await supertest(app)
-    .get('/')
-    .expect(200)
-    .then((response) => {
-      console.log('We recieved a response, ' + response);
-    });
+describe('Webpages', () => {
+  // See that we are getting an index page
+  test('GET /', async () => {
+    await supertest(app)
+      .get('/')
+      .expect(200)
+      .then((response) => {
+        console.log('We recieved a response, ' + response);
+      });
+  });
+
+  // See that we are getting the browse page
+  test('GET /browse', async () => {
+    await supertest(app).get('/browse').expect(200);
+  });
+
+  // See that we are getting the dashboard page
+  test('GET /dashboard', async () => {
+    await supertest(app).get('/dashboard').expect(200);
+  });
+
+  // See that we are getting the login page
+  test('GET /dashboard', async () => {
+    await supertest(app).get('/login').expect(200);
+  });
 });
