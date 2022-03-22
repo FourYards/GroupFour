731e5e440248851bd85c208ac3dc2e168aad3f90
Keatonan <keatona379@gmail.com>
2022-03-21 22:26:03 -0600

    Test that listings can be received by user and by id

--- server/test/api/listings.test.js
+++ server/test/api/listings.test.js
@@ -6,9 +6,20 @@ const app = createServer();
 describe('GET a listing with id 1', () => {
   it('Responds with a json object containing the listing', async () => {
     await supertest(app)
-      .get('/api/listing')
-      .send({ id: 1 })
+      .get('/api/listing?id=1')
       .set('Accept', 'application/json')
       .expect(200);
+  });
+});
+
+describe('GET all listing posted by user with id 1', () => {
+  it('Responds with a json object containing the listings', async () => {
+    await supertest(app)
+      .get('/api/listing?creator_id=1')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+        expect(response.body.data).toBeDefined;
+      });
   });
 });
