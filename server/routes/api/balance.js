8bedac380efa7e461121f0dc8acd6330f1715bac
Keatonan <keatona379@gmail.com>
2022-03-21 19:08:50 -0600

    Make balance router return mock data

--- server/routes/api/balance.js
+++ server/routes/api/balance.js
@@ -3,16 +3,25 @@ const router = express.Router();
 
 /* GET user balance. */
 router.get('/', (req, res, next) => {
-  res.send(
-    'This URL will respond to GET and PUT requests about the balance of the authorized user.'
-  );
+  const mockBalance = {
+    balance: 100,
+  };
+  // TODO validate user is authentic
+
+  // TODO get the users balance from the database model
+  const data = mockBalance;
+  // Send the information back to the client
+  res.json(data);
 });
 
 /* PUT funds into or out of user's account */
 router.put('/', (req, res, next) => {
-  res.send(
-    "This endpoint will update the user's balance to add or remove funds."
-  );
+  // TODO get the right user balance in the database and update it
+  let err = null;
+  if (err) {
+    res.status(200).json({ err: err });
+  }
+  res.status(204).send();
 });
 
 module.exports = router;
