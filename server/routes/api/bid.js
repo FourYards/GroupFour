2e488074608b26eb872a828231639736e4228f7c
Keatonan <keatona379@gmail.com>
2022-03-21 19:43:04 -0600

    Implement get request for bids

--- server/routes/api/bid.js
+++ server/routes/api/bid.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const createError = require('http-errors');
 const router = express.Router();
 
 /* GET one or many bids. */
 router.get('/', (req, res, next) => {
-  res.send(
-    'This URL will respond to GET, POST, and DELETE requests relating to bids.'
-  );
+  const mockData = {
+    data: [
+      {
+        id: req.query.id,
+        bidder_id: 1,
+        amount: 100.0,
+        listing_id: 1,
+      },
+    ],
+  };
+
+  if (req.query.id) {
+    // TODO retreive bid from db by its id
+    const data = mockData;
+    res.json(data);
+  } else if (req.query.order_id) {
+    // TODO retreive bids from db by the order it is associated with
+    const data = mockData;
+    res.json(data);
+  } else if (req.query.bidder_id) {
+    // TODO retreive bids from db by the person who made the bid, select only bids of orders that havent been completed
+    const data = mockData;
+    res.json(data);
+  }
+  next(createError(404));
 });
 
 /* POST a new bid. */
