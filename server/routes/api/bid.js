e7003e6f58a66ebf4c8af836f34d1a1ba334ca0e
Keatonan <keatona379@gmail.com>
2022-03-23 17:36:22 -0600

    Connect get request to database

--- server/routes/api/bid.js
+++ server/routes/api/bid.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db/models');
+const Bid = db.Bid;
 
 /* GET one or many bids. */
-router.get('/', (req, res, next) => {
-  const mockData = {
-    data: [
-      {
-        id: req.query.id,
-        bidder_id: 1,
-        amount: 100.0,
-        order_id: 1,
-      },
-    ],
-  };
-
+router.get('/', async (req, res, next) => {
   if (req.query.id) {
-    // TODO retreive bid from db by its id
-    const data = mockData.data[0];
+    // retreive bid from db by its id
+    const data = await Bid.findByPk(req.query.id);
     res.json(data);
-  } else if (req.query.order_id) {
-    // TODO retreive bids from db by the order it is associated with
-    const data = mockData;
+  } else if (req.query.order) {
+    // retreive bids from db by the order it is associated with
+    const data = await Bid.findAll({
+      where: {
+        order: req.query.order,
+      },
+    });
     res.json(data);
-  } else if (req.query.bidder_id) {
-    // TODO retreive bids from db by the person who made the bid, select only bids of orders that havent been completed
-    const data = mockData;
+  } else if (req.query.bidder) {
+    // retreive bids from db by the person who made the bid, select only bids of orders that havent been completed
+    const data = await Bid.findAll({
+      where: {
+        bidder: req.query.bidder,
+      },
+    });
     res.json(data);
+  } else {
+    res.status(400).json({ err: 'Bad Request' });
   }
-  res.status(400).json({ err: 'Bad Request' });
 });
 
 /* POST a new bid. */
