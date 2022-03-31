0ee3e70a991e88accde577b7c87f574443cfbdcb
Keatonan <keatona379@gmail.com>
2022-03-30 18:32:53 -0600

    Implement post listing page

--- client/page/createListing.vue
+++ client/page/createListing.vue
@@ -2,105 +2,93 @@
   <fragment>
     <div id="createListing" align="center">
       <h1>Post a Listing</h1>
-      <form action="POST" method="/api/listing">
-        <div id="listingDetails">
-          <h6>Details</h6>
 
-          <label for="listingTitle">Job Title:</label>
-          <input type="text" name="listingTitle" id="listingTitle" Autofocus />
-          <br />
-
-          <label for="JobType">Job Type:</label>
-          <select name="JobType" id="JobType">
-            <option value="0">Lawn Mowing</option>
-            <option value="1">Snow Shoveling</option>
-          </select>
-
-          <label for="listingTime">Time Estimate:</label>
-          <input id="listingTime" type="number" min="0" />
-          minutes
-        </div>
-        <div id="listingAddress">
-          <h6>Address</h6>
-
-          <label for="streetAddress">Street Address:</label>
-          <input type="text" name="streetAddress" id="streetAddress" />
-
-          <label for="city">City Name:</label>
-          <input type="text" name="city" id="cityinput" />
-
-          <label for="state">State:</label>
-          <select name="state" id="stateList">
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District Of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-          </select>
-
-          <label for="zipCode">Zip Code:</label>
-          <input
-            type="text"
-            name="zipCode"
-            id="zipCode"
-            pattern="[0-9]*"
-            maxlength="5"
-          />
-        </div>
-        <div id="listingDesc">
-          <h6>Description</h6>
-          <textarea name="desc" id="listingDescription" cols="60" rows="5">
-                    Enter any additional details here
-                </textarea
+      <div id="postlisting">
+        <b-form @submit="onSubmit">
+          <b-form-group
+            id="input-group-1"
+            label="Job Type:"
+            label-for="input-1"
           >
-        </div>
-      </form>
+            <b-form-select id="input-1" v-model="form.job" :options="jobs">
+              >
+            </b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-2"
+            label="Time Estimate:"
+            label-for="input-2"
+          >
+            <b-input-group append="min">
+              <b-form-input
+                id="input-2"
+                v-model="form.lengthMin"
+                placeholder="0"
+                type="number"
+                min="0"
+                required
+              ></b-form-input>
+            </b-input-group>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-3"
+            label="Street Address:"
+            label-for="input-3"
+          >
+            <b-form-input
+              id="input-3"
+              v-model="form.streetAddress"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-4" label="City:" label-for="input-4">
+            <b-form-input
+              id="input-4"
+              v-model="form.city"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-5" label="State:" label-for="input-5">
+            <b-form-select id="input-5" v-model="form.state" :options="states">
+              >
+            </b-form-select>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-6"
+            label="Zip Code:"
+            label-for="input-6"
+          >
+            <b-form-input
+              id="input-6"
+              v-model="form.zipcode"
+              type="text"
+              placeholder="00000"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="input-group-7"
+            label="Description:"
+            label-for="input-7"
+          >
+            <b-form-textarea
+              id="input-7"
+              v-model="form.desc"
+              placeholder="Additional Information"
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Post</b-button>
+        </b-form>
+      </div>
     </div>
   </fragment>
 </template>
@@ -117,6 +105,79 @@ export default {
   data() {
     return {
       //Any variables / data used on the page
+      form: {
+        job: null,
+        lengthMin: 0,
+        streetAddress: null,
+        city: null,
+        state: null,
+        zipcode: null,
+        desc: null,
+      },
+      jobs: [
+        { text: 'Select One', value: null },
+        'Lawn Mowing',
+        'Snow Shoveling',
+      ],
+      states: [
+        { text: 'Select One', value: null },
+        'Alabama',
+        'Alaska',
+        'American Samoa',
+        'Arizona',
+        'Arkansas',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'District of Columbia',
+        'Florida',
+        'Georgia',
+        'Guam',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Minor Outlying Islands',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+        'Northern Mariana Islands',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Pennsylvania',
+        'Puerto Rico',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Vermont',
+        'Virginia',
+        'Washington',
+        'West Virginia',
+        'Wisconsin',
+        'Wyoming',
+      ],
     };
   },
 
@@ -124,10 +185,74 @@ export default {
 
   methods: {
     //Api calls to populate data
+    onSubmit(event) {
+      // Avoid default form submission
+      event.preventDefault();
+
+      // Validate input
+      let errors = [];
+      if (!this.jobs.includes(this.form.job)) {
+        errors.push('Job Type');
+      }
+      if (isNaN(this.form.lengthMin) || this.form.lengthMin < 0) {
+        errors.push('Time Estimate');
+      }
+      if (!this.form.streetAddress) {
+        errors.push('Street Address');
+      }
+      if (!this.form.city) {
+        errors.push('City');
+      }
+      if (!this.states.includes(this.form.state)) {
+        errors.push('State');
+      }
+      if (
+        !this.form.zipcode ||
+        this.form.zipcode.length != 5 ||
+        isNaN(this.form.zipcode)
+      ) {
+        errors.push('Zip Code');
+      }
+
+      if (errors.length > 0) {
+        let err = 'Incorrect entries for: ';
+        for (let i of errors) {
+          err += i + ',\n ';
+        }
+        alert(err);
+        return;
+      }
+
+      // POST the listing to the database
+      const url = '/api/listing';
+      let body = this.form;
+      body['target'] = '/dashboard';
+      fetch(url, {
+        method: 'POST',
+        mode: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        body: JSON.stringify(body),
+      });
+    },
+
+    //TODO request job types from db
   },
 };
 </script>
 
 <style>
 /* Any styles specific to the page */
+h1 {
+  padding: 20px;
+}
+
+#postlisting {
+  align-content: center;
+  padding-top: 3em;
+  padding-left: 30em;
+  padding-right: 30em;
+}
 </style>
