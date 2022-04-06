c248e31849ae978c0e23ec6c860cdf6a093859c3
Keatonan <keatona379@gmail.com>
2022-04-06 12:34:59 -0600

    Add outline for updating user settings

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db/models');
+const UserAccount = db.UserAccount;
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send(
-    'This URL will handle POST, PUT, and DELETE requests relating to user accounts.'
-  );
-});
+/* Function to update user settings 
+req format:
+{
+  model-value: new-value
+}
+Locked values:
+* passwordHash
+*/
+router.patch('/', async (req, res, next) => {
+  let changed = false;
+  for (let key of Object.keys(req.body)) {
+    if (Object.key(UserAccount).contains(key) && key != 'passwordHash') {
+      // TODO conver req.user to a db object
+      const user = UserAccount.findByPk(req.user.id);
+      user[key] = req.body[key];
+      changed = true;
+    }
+  }
 
-/* POST a new User. */
-router.post('/', (req, res, next) => {
-  res.send('This endpoint will try to create a new user.');
-});
-
-/* PUT a new User. */
-router.put('/', (req, res, next) => {
-  res.send('This endpoint will update the users information.');
+  if (changed) {
+    res.status(204);
+  } else {
+    res.status(400);
+  }
 });
 
 module.exports = router;
