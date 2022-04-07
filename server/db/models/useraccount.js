275c7e721b1dfac641caa426b8db90a07937f4eb
Kytech <Kytech@users.noreply.github.com>
2022-04-07 02:42:30 -0600

    Refine relation between roles and user accounts.
    This is how things should be behaving now!

--- server/db/models/useraccount.js
+++ server/db/models/useraccount.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models['Role']);
+      this.belongsTo(models['Role'], { foreignKey: 'role', as: 'roleDetails' });
       this.belongsTo(models['DisplayType']);
 
       this.hasMany(models['Bid'], {
