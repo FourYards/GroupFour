ac58f5d00c01c1b76d351173cc9abd4deb3019ec
Richard Snider <lighthousemaniac@gmail.com>
2022-04-12 17:53:38 +0000

    bids

--- server/db/seeders/20220322034916-users.js
+++ server/db/seeders/20220322034916-users.js
@@ -39,6 +39,16 @@ module.exports = {
           displayType: 'PRO',
           role: 'USR',
         },
+        {
+          // suggestion for name by keaton
+          realName: 'Carrot Cake',
+          emailAddress: 'carrot.cake@usu.edu',
+          passwordHash:
+            '$argon2id$v=19$m=15360,t=3,p=1$qztbmFD2qq0t+rs5PFAvdA$zPubCBzB3sWuBuFw2oY97kk/SsVfYPGRhHgCpHH8fh3f2n1u92xmGI2OV9+OkyWngpjnyuiO4sTwNEyGeQxOiVMXxfSYEn9skshDiwR0BK8TDHdI8ISG8Cvtg0MT89VE1AIoqegwNCkW/Et/PnnKcjM2iLDeMaFImlPM7kFKe/I',
+          phoneNumber: '5432167890',
+          displayType: 'PRO',
+          role: 'USR',
+        },
       ],
       {}
     );
---
+++ server/db/seeders/20220412173640-bids.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Bids',
+      [
+        // on listing 1
+        {
+          amount: 3000,
+          bidder: 3,
+          listing: 1,
+        },
+        {
+          amount: 2500,
+          bidder: 4,
+          listing: 1,
+        },
+        {
+          amount: 1500,
+          bidder: 3,
+          listing: 1,
+        },
+
+        // on listing 2
+        {
+          amount: 1750,
+          bidder: 4,
+          listing: 2,
+        },
+        {
+          amount: 1350,
+          bidder: 3,
+          listing: 2,
+        },
+
+        // on listing 3
+        {
+          amount: 1475,
+          bidder: 4,
+          listing: 3,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Bids', null, {});
+  },
+};
