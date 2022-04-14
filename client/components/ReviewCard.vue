c392e3a3467c3659031eab57b42ca7cd5fdba1ed
Wesley Perrett <wesley.g.perrett@gmail.com>
2022-04-13 20:59:50 -0600

    initial commit for review card

---
+++ client/components/ReviewCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <fragment>
+    <div class="container">
+      <b-card
+        bg-variant="info"
+        text-variant="white"
+        header="Info"
+        class="text-center"
+      >
+        <b-card-text
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text
+        >
+      </b-card>
+    </div>
+  </fragment>
+</template>
+
+<script>
+export default {
+  name: 'example-component',
+  props: {
+    //Data variables
+  },
+  data() {
+    //Data to populate component
+  },
+  computed: {
+    //Computed variables
+  },
+  methods: {
+    //Methods for component
+  },
+};
+</script>
+
+<style scoped></style>
