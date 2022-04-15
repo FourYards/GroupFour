f612209e0c83796162811ee0d8911d919120da16
Wesley Perrett <wesley.g.perrett@gmail.com>
2022-04-15 09:40:52 -0600

    made words strong on dashboard

--- client/components/JobListingCard.vue
+++ client/components/JobListingCard.vue
@@ -16,10 +16,10 @@
               <td>{{ job.description }}</td>
             </tr>
             <tr>
-              <td>Job: {{ job.typeDetails.description }}</td>
+              <td><strong>Job:</strong> {{ job.typeDetails.description }}</td>
             </tr>
             <tr>
-              <td>Posted by: {{ job.creator.realName }}</td>
+              <td><strong>Posted by:</strong> {{ job.creator.realName }}</td>
             </tr>
             <tr>
               <td>{{ job.status }}</td>
