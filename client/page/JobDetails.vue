018ae0069376b180917c50909ad3e3357ce23afd
Keatonan <keatona379@gmail.com>
2022-04-17 15:53:25 -0600

    Give user ability to post listings on any listing

--- client/page/JobDetails.vue
+++ client/page/JobDetails.vue
@@ -21,17 +21,22 @@
       <b-card class="mb-3 b-card">
         <b-card-text> {{ job.date }} </b-card-text>
       </b-card>
+
       <b-card class="mb-3 b-card">
-        <form action="">
-          <b-form-input
-            size="lg"
-            placeholder="Enter Bid Amount"
-            class="mb-3"
-          ></b-form-input>
-          <b-button href="/dashboard" block variant="success"
-            >Place Bid</b-button
-          >
-        </form>
+        <b-form @submit="onSubmit">
+          <b-input-group prepend="$" size="lg" class="mb-3">
+            <b-form-input
+              size="lg"
+              type="number"
+              min="0.00"
+              step="any"
+              placeholder="Enter Bid Amount"
+              class=""
+              v-model="form.amount"
+            ></b-form-input>
+          </b-input-group>
+          <b-button block type="submit" variant="success">Place Bid</b-button>
+        </b-form>
       </b-card>
     </div>
   </fragment>
@@ -51,6 +56,10 @@ export default {
         customer: 'Wesley Perrett',
         description: 'This is a description for a job',
         date: 'March 23, 2021',
+      },
+      form: {
+        amount: 0,
+        order: new URLSearchParams(location.search.substring(1)).get('id'),
       },
     };
   },
@@ -77,6 +86,37 @@ export default {
     //   });
   },
   methods: {
+    onSubmit(event) {
+      event.preventDefault();
+
+      if (isNaN(this.form.amount) || !this.form.amount > 0) {
+        alert('Improper Bid Amount');
+        return;
+      }
+
+      if (!this.form.order) {
+        alert('Unable to Find Listing. Please refresh and try again.');
+        return;
+      }
+
+      // Convert the bid amount to cents
+      this.form.amount *= 100;
+
+      const url = '/api/bid';
+      const body = this.form;
+      const csrfToken = document.querySelector('meta[name=csrf-token]').content;
+      fetch(url, {
+        method: 'POST',
+        mode: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrfToken,
+        },
+        body: JSON.stringify(body),
+      }).then(() => {
+        window.location.replace('/dashboard');
+      });
+    },
     //Api calls to populate data
   },
 };
