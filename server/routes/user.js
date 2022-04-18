c714dc7d79df8ea0f8f3411dc9db85a86cc4d831
Kytech <Kytech@users.noreply.github.com>
2022-04-18 01:49:45 -0600

    Fix broken sign-up page.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+  signupPageController,
+  signupNewUserController,
+} = require('../controllers/login');
+
+router.get('/newSignUp', signupPageController);
+router.post('/newSignUp', signupNewUserController);
+
 router.get('/:UserId', (req, res, next) => {
   // Tell the client that it is looking at someone else's profile
   res.locals.clientContext.userId = req.params.UserId;
